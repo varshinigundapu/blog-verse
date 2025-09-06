a55bd62eaa515fe5e9e17a1c804fe06f4b4f76fe
varshinigundapu <ramayashovarshinigundapu@gmail.com>
2025-09-06 15:54:33 +0530

    functions in js

---
+++ functions.js
@@ -0,0 +1,26 @@
+function greet(){
+    console.log("welcome to functions in js")
+}
+greet()
+function add(x,y)
+{
+    return x+y;
+
+}
+let result=add(18,18)
+console.log(result)
+const pi=3.14
+console.log(pi)
+const multiply=function(x,y)
+{
+    return x*y;
+}
+console.log( multiply(2,5))
+const square=(x)=> x*x;
+console.log(square(5))
+const subtract=(x,y)=>
+{
+    return x-y;
+}
+console.log(subtract(200,100))
+
