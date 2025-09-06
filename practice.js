b9b80a7d883e587c0c5d8263e937dc9a6ac97a1c
varshinigundapu <ramayashovarshinigundapu@gmail.com>
2025-09-06 12:36:45 +0530

    commands

--- practice.js
+++ practice.js
@@ -1,1 +1,26 @@
 console.log("hello world,varshini")
+let name="varshini";
+console.log(name)
+let num=18;
+console.log(num)
+let islogged=false;
+console.log(islogged)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+let x;
+console.log(x)
+console.log(typeof x)
+let y=null;
+console.log(y)
+console.log(typeof y)
+let arr=[1,2,3,4,5,6]
+console.log(arr)
+console.log(typeof arr)
+let person={
+    name:"varshini",
+    age:18
+}
+console.log(person.age)
+console.log(person.name)
+console.log(typeof person)
